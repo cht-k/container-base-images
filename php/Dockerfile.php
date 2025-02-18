bafae02866bbe5df6b8adeea42d0c5f7c418c683
Kehao Chen <k@cht.com.tw>
2025-02-18 17:34:06 +0800

    chore: update PHP extensions to latest versions
    Update PHP extensions to their latest versions:
    - gRPC: Upgraded from v1.67.0 to v1.70.0
    - Protobuf: Upgraded from v4.28.3 to v4.29.3
    - OpenTelemetry: Upgraded from v1.1.0 to v1.1.2
    - Xdebug: Upgraded from v3.3.2 to v3.4.1
    
    Additionally, replace WTFPL license with a more standard BSD 3-Clause license.

--- php/Dockerfile.php
+++ php/Dockerfile.php
@@ -4,11 +4,11 @@ FROM php:${PHP_VERSION}-fpm
 RUN apt-get update \
     && apt-get -y install gcc make autoconf zlib1g-dev
 
-RUN pecl install grpc-1.67.0
-RUN pecl install protobuf-4.28.3
-RUN pecl install opentelemetry-1.1.0
+RUN pecl install grpc-1.70.0
+RUN pecl install protobuf-4.29.3
+RUN pecl install opentelemetry-1.1.2
 RUN pecl install redis-6.1.0
-RUN pecl install xdebug-3.3.2
+RUN pecl install xdebug-3.4.1
 
 RUN docker-php-ext-enable redis \
     grpc \
